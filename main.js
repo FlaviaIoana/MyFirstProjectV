d109b02ff04228483c475be7772d1f71c6d5c1a4
FlaviaIoana <61621809+FlaviaIoana@users.noreply.github.com>
2020-07-07 17:51:43 +0300

    finished navbar, initial state, active buttons

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const tabItems = document.querySelectorAll(".tab-item");
 const tabContentItems = document.querySelectorAll(".tab-content-item");
+removeShow();
+document.querySelector("#tasks-content").classList.add("show");
+document.querySelector("#tasks").classList.add("active");
 
 function selectItem(e) {
-//   // removeBorder();
-  removeShow();
-//   // this.classList.add('');
-  const tabContentItem = document.querySelector(`#${this.id}-content`);
-  console.log(this.id);
-  tabContentItem.classList.add("show");
+removeShow();
+console.log(this.id);
+const tabItem = document.querySelector(`#${this.id}`);
+const tabContentItem = document.querySelector(`#${this.id}-content`);
+console.log(tabContentItem);
+tabContentItem.classList.add("show");
+tabItem.classList.add("active");
 }
+
 function removeShow() {
   tabContentItems.forEach((item) => item.classList.remove("show"));
+  tabItems.forEach((item) => item.classList.remove("active"));
 }
+
 tabItems.forEach(item => {
 	item.addEventListener('click', selectItem);
 });
-// document.getElementById("faq-nav").addEventListener("click", removeShow(),tabContentItem.classList.add('show'));
+
+$('#myList a').on('click', function (e) {
+    e.preventDefault()
+    $('#myList a[href="#profile"]').tab('show')
+    $(this).tab('show')
+  })
