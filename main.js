a9e53429bd917e100aec42bea969ee7fe00cc06e
FlaviaIoana <61621809+FlaviaIoana@users.noreply.github.com>
2020-07-15 16:11:25 +0300

    fixed suggestions

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const tabItems = document.querySelectorAll(".tab-item");
 const tabContentItems = document.querySelectorAll(".tab-content-item");
 const inProgressItems = document.querySelectorAll(".color-c");
-// functia removeShow face sa nu apara continutul tututor taburilor
-removeShow();
-// selecteaza pagina (tabul) care sa se afiseze initial si butonul acesteia activat
-document.querySelector("#tasks-content").classList.add("show");
-document.querySelector("#tasks").classList.add("active");
+
 
 function selectItem(e) {
   removeShow();
@@ -17,53 +13,61 @@ function selectItem(e) {
   tabItem.classList.add("active");
 }
 
-function removeShow() {
+const removeShow = () => {
   tabContentItems.forEach((item) => item.classList.remove("show"));
   tabItems.forEach((item) => item.classList.remove("active"));
 }
-
+removeShow();
+// functia removeShow face sa nu apara continutul tututor taburilor
+document.querySelector("#tasks-content").classList.add("show");
+// selecteaza pagina (tabul) care sa se afiseze initial si butonul acesteia activat
+document.querySelector("#tasks").classList.add("active");
 tabItems.forEach((item) => {
   item.addEventListener("click", selectItem);
 });
 
 // butonul close alaturat fiecarui nod existent
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+const myNodelist = document.getElementsByClassName("color-c");
+const myNodel = document.getElementsByClassName("list");
+// for (let i = 0; i < myNodelist.length; i++) { before
+// myNodelist.forEach((node)=>{ 
+Array.prototype.forEach.call(myNodelist, li => {
+  const spanClose = document.createElement("SPAN");
+  const txtClose = document.createTextNode("\u00D7");
+  spanClose.className = "close";
+  spanClose.appendChild(txtClose);
+  console.log(li);
+  li.appendChild(spanClose);
 }
+)
 
 // functionalitatea butonului close
-function closeFunctionality() {
-  var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
+const closeFunctionality = () => {
+  const close = document.getElementsByClassName("close");
+  Array.prototype.forEach.call(close, node => {
+    node.onclick = function () {
+      const div = this.parentElement;
       div.style.display = "none";
     };
-  }
+  })
 }
 //adaugarea butonului in progress fiecarui item existent
-var inprogress = document.getElementsByClassName("inprogress");
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00AB");
-  span.className = "inprogress";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+const inprogress = document.getElementsByClassName("inprogress");
+Array.prototype.forEach.call(myNodelist, li => {
+  const spanProg = document.createElement("SPAN");
+  const txtProg = document.createTextNode("\u00AB");
+  spanProg.className = "inprogress";
+  spanProg.appendChild(txtProg);
+  li.appendChild(spanProg)
+  console.log(li);
 }
+)
 //functionalitatea butonului "in progress"
-function inprogressFunctionality() {
-  var inprog = document.getElementsByClassName("inprogress");
-  var i;
-  for (i = 0; i < inprog.length; i++) {
-    inprog[i].onclick = function () {
-      var div = this.parentElement;
+const inprogressFunctionality = () => {
+  const inprog = document.getElementsByClassName("inprogress");
+  Array.prototype.forEach.call(inprog, node => {
+    node.onclick = function () {
+      const div = this.parentElement;
       console.log(div);
       //nu se poate ajunge aici decat din starea initiala
       if (div.style.backgroundColor != "rgb(136, 136, 136)") {
@@ -71,84 +75,69 @@ function inprogressFunctionality() {
       }
       // div.classList.add("color-c");     alta modalitate
     };
-  }
+  })
 }
 //butonul finish task, adaugat itemilor existenti
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u2713");
-  span.className = "finish";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+Array.prototype.forEach.call(myNodelist, li => {
+  const spanFinish = document.createElement("SPAN");
+  const txtFinish = document.createTextNode("\u2713");
+  spanFinish.className = "finish";
+  spanFinish.appendChild(txtFinish);
+  li.appendChild(spanFinish);
+})
 //functionalitatea butonului finish
-function finishFunctionality() {
-  var finish = document.getElementsByClassName("finish");
-  var i;
-  for (i = 0; i < finish.length; i++) {
-    finish[i].onclick = function () {
-      var div = this.parentElement;
+const finishFunctionality = () => {
+  const finish = document.getElementsByClassName("finish");
+  Array.prototype.forEach.call(finish, node => {
+    node.onclick = function () {
+      const div = this.parentElement;
       //doar daca a fost in progres inainte
       if (div.style.backgroundColor == "yellow") {
         div.style.backgroundColor = "rgb(136, 136, 136)";
         div.classList.add("checked");
       }
     };
-  }
+  })
 }
-// Nu o folosesc, am pastrat-o ca model pentru toggle
-// var list = document.querySelector("ul");
-// list.addEventListener(
-//   "click",   
-//   function (ev) {
-//     if (ev.target.tagName === "LI") {
-//       // ev.target.classList.toggle("checked");
-//       // ev.target.classList.add("checked");
-//       ev.target.style.backgroundColor= "#888";
-//       ev.target.style.color= "black";
-//       ev.target.style.textDecoration= "line-through";
-//     }
-//   },
-//   false
-// );
-
+function currentNodes(spanx, txt, name, li) {
+  spanx.className = name;
+  spanx.appendChild(txt);
+  li.appendChild(spanx);
+}
 // creeaza un nou nod in lista la apasarea butonului add
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue1 = document.getElementById("myInput").value;
-  var inputValue2 = document.getElementById("myInputContent").value;
-  var inputValue = 'Title:  ' + inputValue1 + ' | Content:' + inputValue2;
-  var t = document.createTextNode(inputValue);
+const newElement = () => {
+  const li = document.createElement("li");
+  let inputValue1 = document.getElementById("input-title").value;
+  let inputValue2 = document.getElementById("input-content").value;
+  const title = "Title";
+  const content = "Content";
+  let inputValue = `${title}: ` + inputValue1 + ` | ${content}: ` + inputValue2;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   if ((inputValue1 === "") && (inputValue2 === "")) {
     // alert("You must write something in at least one input!");
     document.getElementById("demo").innerHTML = "Please write something!";
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("list").appendChild(li);
     document.getElementById("demo").innerHTML = "";
   }
-  document.getElementById("myInput").value = "";
-  document.getElementById("myInputContent").value = "";
-  //close button adaugat noului item
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  //in progress button adaugat noului item
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00AB");
-  span.className = "inprogress";
-  span.appendChild(txt);
-  li.appendChild(span);
-  //finish button adaugat noului item
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u2713");
-  span.className = "finish";
-  span.appendChild(txt);
-  li.appendChild(span);
+  document.getElementById("input-title").value = "";
+  document.getElementById("input-content").value = "";
+  //close button adaugat noului item cu reutilizare de cod
+  let spanCls = document.createElement("SPAN");
+  const txtCls = document.createTextNode("\u00D7");
+  currentNodes(spanCls, txtCls, "close", li);
+
+  //in progress button adaugat noului item cu reutilizare de cod
+  const spanPrg = document.createElement("SPAN");
+  const txtPrg = document.createTextNode("\u00AB");
+  currentNodes(spanPrg, txtPrg, "inprogress", li);
+
+  //finish button adaugat noului item cu reutilizare de cod
+  const spanFin = document.createElement("SPAN");
+  const txtFin = document.createTextNode("\u2713");
+  currentNodes(spanFin, txtFin, "finish", li)
+
   closeFunctionality();
   inprogressFunctionality();
   finishFunctionality();
