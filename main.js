69edf94f8ed1f5d9a395d9a44801ade467537dbd
FlaviaIoana <61621809+FlaviaIoana@users.noreply.github.com>
2020-07-15 08:38:14 +0300

    contact page, form with validations

--- main.js
+++ main.js
@@ -27,36 +27,35 @@ tabItems.forEach((item) => {
 });
 
 // butonul close alaturat fiecarui nod existent
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+const myNodelist = document.getElementsByClassName("color-c");
+for (let i = 0; i < myNodelist.length; i++) {
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 function myEdit() {
   for (i = 0; i < myNodelist.length; i++) {
-    var edit = document.createElement("SPAN");
-    var eText = document.createTextNode("\u270E");
+    const edit = document.createElement("SPAN");
+    const eText = document.createTextNode("\u270E");
     edit.className = "edit";
     edit.appendChild(eText);
     myNodelist[i].appendChild(edit);
     edit.onclick = function merge() {
-      var entryy = this.parentElement.childNodes[0];
-      var res = entryy.data.split(" | ");
-      var TitleText = res[0].substring(7);
-      var n = res[1].indexOf("Content: ");
-      var ContentText = res[1].substring(n + 9);
+      let entryy = this.parentElement.childNodes[0];
+      let res = entryy.data.split(" | ");
+      let TitleText = res[0].substring(7);
+      let n = res[1].indexOf("Content: ");
+      let ContentText = res[1].substring(n + 9);
       console.log(TitleText + '\n' + ContentText);
       editt = document.getElementById("editt");
       document.getElementById("myInput").value = TitleText;
       document.getElementById("myInputContent").value = ContentText;
 
       editt.onclick = function () {
-        var inputValue01 = document.getElementById("myInput").value;
-        var inputValue02 = document.getElementById("myInputContent").value;
+        let inputValue01 = document.getElementById("myInput").value;
+        let inputValue02 = document.getElementById("myInputContent").value;
         if (inputValue01 == "") {
           document.getElementById("demo").innerHTML = "Please write a title!";
         }
@@ -85,37 +84,35 @@ function myEdit() {
 }
 // functionalitatea butonului close
 function closeFunctionality() {
-  var close = document.getElementsByClassName("close");
-  var i;
+  const close = document.getElementsByClassName("close");
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     };
   }
 }
 //adaugarea butonului in progress fiecarui item existent
-var inprogress = document.getElementsByClassName("inprogress");
+let inprogress = document.getElementsByClassName("inprogress");
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00AB");
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00AB");
   span.className = "inprogress";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 //functionalitatea butonului "in progress"
 function inprogressFunctionality() {
-  var inprog = document.getElementsByClassName("inprogress");
-  var i;
+  let inprog = document.getElementsByClassName("inprogress");
   for (i = 0; i < inprog.length; i++) {
     inprog[i].onclick = function () {
-      var div = this.parentElement;
+      let div = this.parentElement;
       console.log(div);
       //nu se poate ajunge aici decat din starea initiala
       if (div.style.backgroundColor != "rgb(136, 136, 136)") {
         div.style.backgroundColor = "yellow";
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u2713");
+        let span = document.createElement("SPAN");
+        let txt = document.createTextNode("\u2713");
         span.className = "finish";
         span.appendChild(txt);
         div.appendChild(span);
@@ -127,11 +124,10 @@ function inprogressFunctionality() {
 }
 //functionalitatea butonului finish
 function finishFunctionality() {
-  var finish = document.getElementsByClassName("finish");
-  var i;
+  let finish = document.getElementsByClassName("finish");
   for (i = 0; i < finish.length; i++) {
     finish[i].onclick = function () {
-      var div = this.parentElement;
+      let div = this.parentElement;
       //doar daca a fost in progres inainte
       if (div.style.backgroundColor == "yellow") {
         div.style.backgroundColor = "rgb(136, 136, 136)";
@@ -140,31 +136,16 @@ function finishFunctionality() {
     };
   }
 }
-// Nu o folosesc, am pastrat-o ca model pentru toggle
-var list = document.querySelector("ul");
-list.addEventListener(
-  "click",
-  function (ev) {
-    if (ev.target.tagName === "LI") {
-      var yes = ev.target.childNodes[0];
-      var res = yes.data.split(" | ");
-      var TitleText = res[0].substring(7);
-      var ContentText = res[1].substring(9);
-      console.log(TitleText);
-      console.log(ContentText);
-      document.getElementById("myInput").innerHTML = TitleText;
-    }
-  },
-  false
-);
 
 // creeaza un nou nod in lista la apasarea butonului add
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue1 = document.getElementById("myInput").value;
-  var inputValue2 = document.getElementById("myInputContent").value;
-  var inputValue = 'Title:  ' + inputValue1 + ' | Content:' + inputValue2;
-  var t = document.createTextNode(inputValue);
+  const li = document.createElement("li");
+  let inputValue1 = document.getElementById("myInput").value;
+  let inputValue2 = document.getElementById("myInputContent").value;
+  const title = "Title";
+  const content="Content";
+  let inputValue = `${title}: ` + inputValue1 + ` | ${content}: ` + inputValue2;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue1 === "") {
     // alert("You must write something in at least one input!");
@@ -176,17 +157,17 @@ function newElement() {
   document.getElementById("myInput").value = "";
   document.getElementById("myInputContent").value = "";
   //close button adaugat noului item
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
   //in progress button adaugat noului item
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00AB");
-  span.className = "inprogress";
-  span.appendChild(txt);
-  li.appendChild(span);
+  const span2 = document.createElement("SPAN");
+  const txt2 = document.createTextNode("\u00AB");
+  span2.className = "inprogress";
+  span2.appendChild(txt2);
+  li.appendChild(span2);
   closeFunctionality();
   inprogressFunctionality();
   myEdit();
@@ -196,3 +177,52 @@ inprogressFunctionality();
 finishFunctionality();
 myEdit();
 $('#myUL').sortable();
+const signIn = document.getElementById("signIn");
+
+
+  signIn.onclick = function () {
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("inputEmail").value;
+    let subjects = document.getElementById("subjects").value;
+    let message = document.getElementById("message").value;
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const messageFormat=/^.{10,}$/;
+    const info = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      subjects: subjects,
+      message: message,
+      // print: function() {
+      //   alert(this.firstName);
+      // }
+    };
+    // var n = email.indexOf("@"); 
+    const firstN="First name";
+    const lastN="Last name";
+    const emaill="Email";
+    const subjectss="Subjects"
+    const messagee="Message"
+    if(email.match(mailFormat) && message.match(messageFormat))
+    {
+      document.getElementById("informations").innerHTML= `{&quot${firstN}&quot: ` + info.firstName+ "<br />"+ `&quot${lastN}&quot: ` + info.lastName+
+      "<br />"+ `&quot${emaill}&quot: ` + info.email+"<br />"+ `&quot${subjectss}&quot: ` + info.subjects+ "<br />"+`&quot${messagee}&quot: ` + info.message+ "<br />"+`}`;
+      // firstName+'   '+ lastName+'   '+email+'   '+subjects + ' ' + message;
+      document.getElementById("firstName").value="";
+      document.getElementById("lastName").value="";
+      document.getElementById("inputEmail").value="";
+      document.getElementById("subjects").value="";
+      document.getElementById("message").value="";
+    }   
+    else if (email.match(mailFormat)){
+      document.getElementById("informations").innerHTML="Mesajul dvs trebuie sa contina cel putin 10 caractere";
+    }
+    else if (message.match(messageFormat)){
+      document.getElementById("informations").innerHTML="Email-ul este invalid!";
+    }
+    else{
+      document.getElementById("informations").innerHTML="Mesajul dvs trebuie sa contina 10 caractere, iar mailul trebuie sa aiba format corespunzator"
+    }
+  }
+  
