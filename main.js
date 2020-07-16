6f1c102db4ec11975b1af8c8a64cd7b5ecde101b
FlaviaIoana <61621809+FlaviaIoana@users.noreply.github.com>
2020-07-16 16:13:01 +0300

    FAQ page

--- main.js
+++ main.js
@@ -220,3 +220,17 @@ signIn.onclick = function () {
     document.getElementById("informations").innerHTML = "Mesajul dvs trebuie sa contina 10 caractere, iar mailul trebuie sa aiba format corespunzator"
   }
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activeCollapsible");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
